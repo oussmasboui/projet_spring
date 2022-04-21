c608708ba66cf12737e2894707e563f68a944724
asma-abessi <asma.abessi@esprit.tn>
2022-04-22 01:10:35 +0200

    Update TravelRestController.java

--- src/main/java/tn/esprit/spring/controllers/TravelRestController.java
+++ src/main/java/tn/esprit/spring/controllers/TravelRestController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import tn.esprit.spring.service.ITravelService;
 
 @RestController 
 @RequestMapping("/travel") 
+@CrossOrigin(origins = "*")
 public class TravelRestController {
 
 	@Autowired
